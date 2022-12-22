630a3bf568bff582a2821b31c317d78453ca2c7d
Altynay Kadyrbek <altynai.kadyrbek@gmail.com>
2022-12-22 14:45:21 +0600

    chapter 5

---
+++ cmd/api/movies.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"greenlight.altynaykadyrbek.net/internal/data"
+	"greenlight.altynaykadyrbek.net/internal/validator"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier). This struct will be our *target
+	// decode destination*.
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"` // Make this field a data.Runtime type.
+		Genres  []string     `json:"genres"`
+	}
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	// If this returns an error we send the client the error message along with a 400
+	// Bad Request status code, just like before.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		// Use the new badRequestResponse() helper.
+		app.badRequestResponse(w, r, err)
+		return
+
+	}
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator.
+	v := validator.New()
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+
+}
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
+// the interpolated "id" parameter from the current URL and include it in a placeholder
+// response.
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	// We can then use the ByName() method to get the value of the "id" parameter from
+	// the slice. In our project all movies will have a unique positive integer ID, but
+	// the value returned by ByName() is always a string. So we try to convert it to a
+	// base 10 integer (with a bit size of 64). If the parameter couldn't be converted,
+	// or is less than 1, we know the ID is invalid so we use the http.NotFound()
+	// function to return a 404 Not Found response.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Create a new instance of the Movie struct, containing the ID we extracted from
+	// the URL and some dummy data. Also notice that we deliberately haven't set a
+	// value for the Year field.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
