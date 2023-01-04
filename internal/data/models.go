3bfab1c6a2ad7ece814fab3e39f741e7feb5dd8b
Altynay Kadyrbek <altynai.kadyrbek@gmail.com>
2023-01-04 16:50:14 +0600

    chapter 8-9

---
+++ internal/data/models.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found ")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+// Create a Models struct which wraps the MovieModel. We'll add other models to this,
+// like a UserModel and PermissionModel, as our build progresses.
+type Models struct {
+	Movies interface {
+		Insert(movie *Movie) error
+		Get(id int64) (*Movie, error)
+		Update(movie *Movie) error
+		Delete(id int64) error
+		GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
+	}
+}
+
+// For ease of use, we also add a New() method which returns a Models struct containing
+// the initialized MovieModel.
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
